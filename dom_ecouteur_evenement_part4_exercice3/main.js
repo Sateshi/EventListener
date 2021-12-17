104e1dd4c7a129eb000e1b769f3e4313e48c69cc
Sateshi <sateshiplay@gmail.com>
2021-12-17 09:07:31 +0100

    fini exo 4 JB

--- dom_ecouteur_evenement_part4_exercice3/main.js
+++ dom_ecouteur_evenement_part4_exercice3/main.js
@@ -11,7 +11,31 @@ p.classList.toggle('text-style');
 
 let span = document.querySelectorAll("p span");
 
+span[0].addEventListener("click",function(){
+    span[0].style.fontWeight = "bolder";
+})
+span[1].addEventListener("click",function(){
+    span[1].style.fontWeight = "bolder";
+})
+span[2].addEventListener("click",function(){
+    span[2].style.fontWeight = "bolder";
+})
 
+span[3].addEventListener("click",function(){
+    span[3].classList.toggle("bolder-red");
+    span[4].classList.remove("bolder-red");
+    span[5].classList.remove("bolder-red");
+})
+span[4].addEventListener("click",function(){
+    span[4].classList.toggle("bolder-red");
+    span[3].classList.remove("bolder-red");
+    span[5].classList.remove("bolder-red");
+})
+span[5].addEventListener("click",function(){
+    span[5].classList.toggle("bolder-red");
+    span[3].classList.remove("bolder-red");
+    span[4].classList.remove("bolder-red");
+})
 
 
 
